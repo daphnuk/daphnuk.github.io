ee28b40a05a6b79ccb3943a8b3393e355822a06a
daphnuk <74135638+daphnuk@users.noreply.github.com>
2023-02-07 12:46:41 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,18 +1,7 @@
 
-<html>
-    <head>
-        <title>Running a Python script</title>
-        <?PHP
-        echo shell_exec("python script.py");
-        ?>
-        </head>
-    <body>
-        <!-- BODY -->
+
 a = 2000
 b = 21
 print(f"a = {a}")
 print(f"b = {b}")
 print(f"a + b = {a + b}")
-
-    </body>
-</html>
